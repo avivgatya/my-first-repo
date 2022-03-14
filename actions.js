9e6a0136ce809316c467368fedc05c0bfe345ee0
avivgatya <101098311+avivgatya@users.noreply.github.com>
2022-03-14 12:04:58 +0200

    14mars000

---
+++ actions.js
@@ -0,0 +1,11 @@
+const { response } = require('express');
+const express = require('express');
+const router = express.Router();
+router.get('greeting',(req,res)=>
+{
+    return res.status(200).json(
+        {
+            message: 'Welcome to my node.js API'
+        }
+    );
+});
